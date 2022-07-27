78ca90c74126eb516fa9e45522249938d3d7844a
ANT_SHUR <shurupov.tosha93@gmail.com>
2022-07-27 22:40:49 +0300

    add small corrections

--- games/game-calc.js
+++ games/game-calc.js
@@ -14,14 +14,14 @@ export const calculation = () => {
   const produceCalc = (num1, num2, operat) => {
     switch(operat) {
         case '+': 
-          return num1 + num2
-          break
+          return num1 + num2;
+          break;
         case '-': 
-          return num1 - num2
-          break
+          return num1 - num2;
+          break;
         case '*': 
-          return num1 * num2
-          break
+          return num1 * num2;
+          break;
       
         default:
             return false;
