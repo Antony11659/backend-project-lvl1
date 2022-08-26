f0e83b0b7d719a533315453b5779950a564d62d8
ANT_SHUR <shurupov.tosha93@gmail.com>
2022-08-26 17:13:43 +0300

    get rid of the lint mistake

--- src/libraryGames.js
+++ src/libraryGames.js
@@ -1,3 +1,3 @@
+/* eslint-disable no-eval */
 export const makeRandomNum = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
-/* eslint-disable */
 export const makeArray = (...arg) => arg;
